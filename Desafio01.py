011dbdb02354decf25a83c41a1fe56cbb64ad134
Mateus Zamorano <52588123+mateuszamorano@users.noreply.github.com>
2024-10-06 17:17:28 -0300

    Desafio01.py

--- Desafio01.py
+++ Desafio01.py
@@ -1,35 +1,63 @@
 N = ["", "", "", "", ""]
 S = ["", "", "", "", ""]
 tam = len(N)
+cont = 0
+opcao = int(input("Bem vindo ao Menu!\n"
+                      "Digite '1' para cadastrar ou '2' para login: "))
 
+while opcao == 1 or opcao == 2:
+    cont += 3
 
-opcao = input("Bem vindo ao Menu!\n"
-              "Digite '1' para cadastrar ou '2' para login: ")
+    if opcao == 2:
+        print("Você ainda não fez cadastro! Faça abaixo;")
 
-if opcao != 1 and opcao != 2:
+    for z in range(tam):
+        usuario = N[z]
+        senha = S[z]
+        N[z] = input("Digite o nome dos usuário: ")
+        S[z] = input("Digite a senha dos usuário: ")
+        print("Usuários cadastrado! Cadastre o próximo;")
+    print("Cadastramento finalizado! Siga o próximo passo;")
 
-for z in range(tam):
+    usu1 = f"O primeiro usuário: {N[0]}. A senha do mesmo: {S[0]}"
+    usu2 = f"O segundo usuário: {N[1]}. A senha do mesmo: {S[1]}"
+    usu3 = f"O terceiro usuário: {N[2]}. A senha do mesmo: {S[2]}"
+    usu4 = f"O quarto usuário: {N[3]}. A senha do mesmo: {S[3]}"
+    usu5 = f"O quinto usuário: {N[4]}. A senha do mesmo: {S[4]}"
 
-    N[z] = input("Digite o nome dos usuário: ")
-    S[z] = input("Digite a senha dos usuário: ")
+    usuario = input("Digite o nome do usuário previamente cadastrado: ")
+    senha = input("Digite a senha do usuário previamente cadastrada: ")
 
-while opcao == 1:
-
-    if login == N[0] and senha == S[0]:
-        print("Você entrou com sucesso!")
-    elif login == N[1] and senha == S[1]:
-        print("Você entrou com sucesso!")
-    elif login == N[2] and senha == S[2]:
-        print("Você entrou com sucesso!")
-    elif login == N[3] and senha == S[3]:
-        print("Você entrou com sucesso!")
-    elif login == N[4] and senha == S[4]:
-        print("Você entrou com sucesso!")
+    if usuario == N[0] and senha == S[0]:
+        print(f"Seja bem vindo Sr(a). {usuario}, login efetuado com sucesso! \nDados cadastrados: "
+                  f"{usu1} \n{usu2} \n{usu3} \n{usu4} e \n{usu5}")
     else:
-        print("Login ou senha inseridos está errado!")
+        print("Senha errada, tente novamente!")
 
-print(F"O primeiro usuário: {N[0]}. A senha do mesmo: {S[0]}"
-      F"\nO segundo usuário: {N[1]}. A senha do mesmo: {S[1]}"
-      F"\nO terceiro usuário: {N[2]}. A senha do mesmo: {S[2]}"
-      F"\nO quarto usuário: {N[3]}. A senha do mesmo: {S[3]}"
-      F"\nO quinto usuário: {N[4]}. A senha do mesmo: {S[4]}")
+    if usuario == N[1] and senha == S[1]:
+        print(f"Seja bem vindo Sr(a). {usuario}, login efetuado com sucesso!  \nDados cadastrados: "
+                  f"{usu1} \n{usu2} \n{usu3} \n{usu4} e \n{usu5}")
+    else:
+        print("Senha errada, tente novamente!")
+
+    if usuario == N[2] and senha == S[2]:
+        print(f"Seja bem vindo Sr(a). {usuario}, login efetuado com sucesso!  \nDados cadastrados: "
+                  f"{usu1} \n{usu2} \n{usu3} \n{usu4} e \n{usu5}")
+    else:
+        print("Senha errada, tente novamente!")
+
+    if usuario == N[3] and senha == S[3]:
+        print(f"Seja bem vindo Sr(a). {usuario}, login efetuado com sucesso! \nDados cadastrados: "
+                  f"{usu1} \n{usu2} \n{usu3} \n{usu4} e \n{usu5}")
+    else:
+        print("Senha errada, tente novamente!")
+
+    if usuario == N[4] and senha == S[4]:
+        print(f"Seja bem vindo Sr(a). {usuario}, login efetuado com sucesso! \nDados cadastrados: "
+                  f"{usu1} \n{usu2} \n{usu3} \n{usu4} e \n{usu5}")
+    else:
+        print("Senha errada, tente novamente!")
+
+    proc = int(input("Deseja encerrar o sistema? Se sim, pressione a tecla '1', caso contrário pressione '2': "))
+    if proc == 1:
+        break
