c8483a99180733a61e4e51bb5bf3eef38127bfdd
Mateus Zamorano <52588123+mateuszamorano@users.noreply.github.com>
2024-10-10 08:19:03 -0300

    Biblioteca.py

--- Biblioteca.py
+++ Biblioteca.py
@@ -14,3 +14,22 @@ def vogais(texto):
         if x in "aeiouAEIOU":
             cont += 1
     print(f"O texto digitado tem {cont} vogais.")
+def inverterNomes(nome1, nome2, nome3, nome4, nome5):
+    nomes = [""]*5
+    nomes[0]=nome1
+    nomes[1]=nome2
+    nomes[2]=nome3
+    nomes[3]=nome4
+    nomes[4]=nome5
+    print(nomes)
+    nomes.reverse()
+    print(nomes)
+def somar(numeros):
+    t = len(numeros)
+    soma=0
+    for x in range(t):
+        soma = soma + numeros[x]
+    print(numeros)
+
+def quant(texto):
+    text = input("Insira um texto para saber quantos caractéres tem: ")
