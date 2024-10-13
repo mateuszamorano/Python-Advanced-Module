791c312dbb0351c236ea54bcb9a97270acdb641b
Mateus Zamorano <52588123+mateuszamorano@users.noreply.github.com>
2024-10-13 13:17:16 -0300

    biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -1,18 +1,27 @@
-class Pessoa:
-    def __init__(self, nome, peso, idade, falando, dormindo, comendo = False):
+class Pessoa: #não concluído
+    def __init__(self, nome, peso, idade, dormindo = False, comendo = True, falando = True):
         self.nome = nome
         self.peso = peso
         self.idade = idade
         self.falando = falando
         self.dormindo = dormindo
         self.comendo = comendo
-    def falar(self):
-        if self.comendo and self.dormindo == True:
-            print(f"{self.nome} não está falando!")
+        if self.idade <= 2:
+            print(f"{self.nome} ainda é um bebê, ou seja, não fala!")
+        else:
+            print(f"{self.nome} tem {self.idade} anos.")
+        if self.dormindo == "Sim" or "sim" and self.comendo == "Não" or "não" and self.falando == "Não" and "não":
+            print(f"O bebê está dormindo!")
+        elif self.dormindo == "Não" or "não" and self.comendo == "Sim" or "sim" and self.falando == "Não" and "não":
+            print("A pessoa está comendo!")
+        elif self.dormindo == "Não" or "não" and self.comendo == "Não" or "não" and self.falando == "Sim" and "sim" and self.idade >= 2:
+            print("A pessoa está falando!")
+            
+    def dormir(self):
+        pass
 
     def comer(self):
-        print(f"{self.nome} está comendo!")
+        pass
 
-    def dormir(self):
-        #acordar
-        print(f"{self.nome} está dormindo!")
+    def falar(self):
+        pass
